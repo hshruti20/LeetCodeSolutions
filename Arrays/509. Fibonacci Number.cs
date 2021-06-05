1cffdf8418fa910ddef7ce20d4b35b1ddfb6e076
hshruti20 <hastak.shruti20@gmail.com>
2021-06-05 23:23:01 +0530

    Create 509. Fibonacci Number.cs

---
+++ Arrays/509. Fibonacci Number.cs
@@ -0,0 +1,46 @@
+﻿/**
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+*/
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciNumber
+{
+    class FibonacciNumber
+    {
+        public int Fib(int n) {
+        
+          int firstnumber = 0, secondnumber = 1, result = 0;  
+   
+            if (n == 0) return 0; //To return the first Fibonacci number   
+            if (n == 1) return 1; //To return the second Fibonacci number   
+   
+   
+            for (int i = 2; i <= n; i++)  
+            {  
+                result = firstnumber + secondnumber;  
+                firstnumber = secondnumber;  
+                secondnumber = result;  
+            }  
+   
+            return result;  
+        
+    }
+    }
+}
