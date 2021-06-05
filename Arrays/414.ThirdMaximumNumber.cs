b98bb21948dcd471fe291e2e32f1afe7b3cfbaac
hshruti20 <hastak.shruti20@gmail.com>
2021-06-05 23:34:22 +0530

    Changed File names and added new class

---
+++ Arrays/414.ThirdMaximumNumber.cs
@@ -0,0 +1,31 @@
+﻿/**
+Given integer array nums, return the third maximum number in this array. If the third maximum does not exist, return the maximum number.
+Example 1:
+
+Input: nums = [3,2,1]
+Output: 1
+Explanation: The third maximum is 1.
+*/
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdMaximumNumber
+{
+    class ThirdMaximumNumber
+    {
+          public int ThirdMax(int[] nums) {
+         nums = nums.OrderByDescending(n=>n).Distinct().ToArray();
+            if (nums.Length < 3)
+                return nums[0];
+
+            return nums[2];
+    }
+    }
+}
