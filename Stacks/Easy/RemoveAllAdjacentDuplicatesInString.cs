2b9aa3d94e49ca5dcc23a8f4ea785e8b1ba8ca52
hshruti20 <hastak.shruti20@gmail.com>
2021-06-16 21:34:55 +0530

    Stack Easy Problems from Leetcode

---
+++ Stacks/Easy/RemoveAllAdjacentDuplicatesInString.cs
@@ -0,0 +1,65 @@
+﻿/**
+1047.
+You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+
+We repeatedly make duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "abbaca"
+Output: "ca"
+Explanation: 
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+Example 2:
+
+Input: s = "azxxzy"
+Output: "ay"
+*/
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    class RemoveAllAdjacentDuplicatesInString
+    {
+        public string RemoveDuplicates(string s)
+        {
+            Stack<char> stk = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (stk.Count > 0)
+                {
+                    char c = stk.Peek();
+                    if (c == s[i])
+                    {
+                        stk.Pop();
+                    }
+                    else
+                        stk.Push(s[i]);
+                }
+                else
+                    stk.Push(s[i]);
+            }
+
+            string a = string.Empty;
+            foreach (var item in stk.Reverse())
+            {
+                a += item;
+            }
+
+            return a;
+
+        }
+    }
+}
---
+++ Stacks/Easy/RemoveOutermostParentheses.cs
@@ -0,0 +1,77 @@
+﻿/**
+1021.
+A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+
+A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A+B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string s, consider its primitive decomposition: s = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+
+Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
+
+ 
+
+Example 1:
+
+Input: s = "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: s = "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: s = "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+*/
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    class RemoveOutermostParentheses
+    {
+        public string RemoveOuterParentheses(string s)
+        {
+            Stack<char> stk = new Stack<char>();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var c in s)
+            {
+                if (stk.Count == 0)
+                    stk.Push(c);
+                else
+                {
+                    if (c == '(')
+                    {
+                        stk.Push(c);
+                        sb.Append(c);
+                    }
+                    else
+                    {
+                        if (stk.Count > 1)
+                            sb.Append(c);
+                        stk.Pop();
+                    }
+                }
+
+            }
+            return sb.ToString();
+        }
+    }
+}
